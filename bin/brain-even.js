789c22f4e71e484795355f3a68bda1d9dda292b9
Lera <lera_muza00@mail.ru>
2024-12-24 23:19:14 +0200

    game add

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,17 +5,17 @@ const getRandomNumber = () => {
 const isEven = (number) => {
     return number % 2 === 0;
 }
-
+import readlineSync from 'readline-sync';
 const startGame = () => {
     console.log("Welcome to the Brain Games!");
-    const userName = prompt('May I have your name? ');
+    const userName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${userName}!`);
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
     let correctAnswersCount = 0;
     while (correctAnswersCount < 3) {
         const randomNumber = getRandomNumber();
         const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-        const userAnswer = prompt(`Question: ${randomNumber}\nYour answer:`).toLowerCase();
+        const userAnswer = readlineSync.question(`Question: ${randomNumber}\nYour answer:`).toLowerCase();
         if (userAnswer === correctAnswer) {
             console.log("Correct!");
             correctAnswersCount++; 
@@ -26,5 +26,5 @@ const startGame = () => {
         }
     }
     console.log(`Congratulations, ${userName}!`);
-}
+};
 startGame();
