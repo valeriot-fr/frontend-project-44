cfa4f06d88bffe1c2269516f05ae956b32143f76
Lera <lera_muza00@mail.ru>
2025-01-11 16:47:33 +0200

    change progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,15 +1,17 @@
 import game from '../index.js';
 
-const generateProgression = (length, start, step) => {
+generateProgression = () => {
+  const length = Math.floor(Math.random() * 6) + 5;
+  const start = Math.floor(Math.random() * 10) + 1;
+  const step = Math.floor(Math.random() * 5) + 1;
+
   const progression = [];
   for (let i = 0; i < length; i += 1) {
     progression.push(start + i * step);
   }
-
   const missingIndex = Math.floor(Math.random() * length);
   const correctAnswer = progression[missingIndex];
   progression[missingIndex] = '..';
-
   return [correctAnswer, progression.join(' ')];
 };
 
