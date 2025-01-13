9cd3d0d55f9555ded7d02cec0082a9f5b8a21802
Lera <lera_muza00@mail.ru>
2025-01-13 14:38:02 +0300

    linter errors

--- src/randomNum.js
+++ src/randomNum.js
@@ -1,6 +1,5 @@
-const getRandomNumber = (min = 1, max = 10) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomNumber = (min = 1, max = 10)=>{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
 export default getRandomNumber;
 
